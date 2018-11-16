11921aa3af22d97642b5c0dc6450abc916aacc56
web-du <1605993932@qq.com>
2018-11-16 18:39:10 +0800

    新增至首页新闻模块

--- js/public.js
+++ js/public.js
@@ -1,6 +1,21 @@
 $(function(){
 //导航下拉
-    $(".header .nav .list li a").hover(function(){
-        $(".header .s-nav").slideDown();
+    $(".header").hover(function(e){
+        $(".header .s-nav").stop(true,true).slideUp();
+
+        $(".header .nav .list li").hover(function(e){
+            var e = window.event || e;
+            e.stopPropagation();
+            $(".header .s-nav").slideDown();
+        })
+    })
+
+    //鼠标移至二级导航
+    $(".header .s-nav .list li").hover(function(){
+        var index = $(this).index();
+        $(".header .nav .list li").eq(index).find("a").addClass("now");
+    },function(){
+        var index = $(this).index();
+        $(".header .nav .list li").eq(index).find("a").removeClass("now");
     })
 })
